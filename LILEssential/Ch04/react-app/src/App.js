924d6104e1ac33dbfaf786290b4a42fac59cb9de
CHRISLAPTOP\choco <chocolat_chips1@yahoo.ca>
2024-07-30 14:01:40 -0600

    Finish chapter 6

--- LILEssential/Ch04/react-app/src/App.js
+++ LILEssential/Ch04/react-app/src/App.js
@@ -9,18 +9,104 @@ function useInput(initialValue){
   ];
 }
 
+function GithubUser( { name, bio, avatar } ){
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{bio}</p>
+      <img src={avatar} alt={`${name}'s avatar`} height={150}/>
+    </div>
+  );
+}
+
+function Lift( {name, elevationGain, status } ){
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{elevationGain} {status}</p>
+    </div>
+  )
+}
+
+const query = `
+query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}`;
+
+const opts = {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify( { query } )
+}
+
+const tahoe_peaks = [
+  {
+    name: "Freel", elevation: 10891
+  },
+  {
+    name: "Monument", elevation:10067
+  },
+  {
+    name: "Pyramid", elevation: 9983
+  },
+  {
+    name: "Tallac", elevation: 9735
+  }
+];
+
+function List( { data, renderItem, renderEmpty } ) {
+  return !data.length ? renderEmpty 
+  : (
+  <ul>
+    {data.map((item) => (
+      <li key = {item.name}>
+        {renderItem(item)}
+      </li>
+    ))}
+  </ul>
+  );
+}
+
+
 function App() {
   const [emotion, setEmotion] = useState("happy");
   const [secondary, setSecondary] = useState("tired");
   const [checked, setChecked] = useReducer(checked => !checked, false);
 
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  }, [emotion]);
+  // useEffect(() => {
+  //   console.log(`It's ${emotion} right now`);
+  // }, [emotion]);
+
+  // useEffect(() => {
+  //   console.log(`It's ${secondary} around here!`);
+  // }, [secondary]);
+
+  const [data, setData] = useState(null);
+  const [liftData, setLiftData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
+    setLoading(true);
+    fetch('https://api.github.com/users/chocolatchips')
+    .then((response) => response.json())
+    .then(setData)
+    .catch(setError);
+  }, []);
+
+  useEffect(() =>{
+    setLoading(true);
+    fetch(`https://snowtooth.moonhighway.com/`, opts)
+    .then((response) => response.json())
+    .then(setLiftData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
 
   const [titleProps, resetTitle] = useInput("");
   const [colorProps, resetColor] = useInput("#000000");
@@ -32,8 +118,29 @@ function App() {
     resetColor();
   };
 
+  if (loading)
+    return (<h1>Loading...</h1>);
+  if (error)
+    return (<pre>{JSON.stringify(error)}</pre>);
+  if (!data) return null;
+
   return (
     <div className="App">
+      <GithubUser
+        name={data.name}
+        bio={data.bio}
+        avatar={data.avatar_url}
+      />
+      <List 
+      data={tahoe_peaks} 
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation} ft.
+        </>
+      )}
+      />
+
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
@@ -65,6 +172,14 @@ function App() {
         />
         <button>ADD</button>
       </form>
+
+        {liftData.data.allLifts.map((lift) => (
+          <Lift
+            name={lift.name}
+            elevationGain={lift.elevationGain}
+            status={lift.status}
+          />
+        ))}
     </div>
   );
 
